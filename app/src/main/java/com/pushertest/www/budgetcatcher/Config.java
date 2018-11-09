6423a7da2deb92f9fbfa2dd9b4edc55a71ec1fff
kaizer1161 <mir.rayan1@gmail.com>
2018-11-10 03:58:24 +0600

    MainActivity navigation fragment views added

---
+++ app/src/main/java/com/pushertest/www/budgetcatcher/Config.java
@@ -0,0 +1,10 @@
+package com.pushertest.www.budgetcatcher;
+
+public class Config {
+
+    public static final String TAG_HOME_FRAGMENT = "TAG_HOME_FRAGMENT";
+    public static final String TAG_CATCHER_FRAGMENT = "TAG_CATCHER_FRAGMENT";
+    public static final String TAG_MANAGE_FRAGMENT = "TAG_MANAGE_FRAGMENT";
+    public static final String TAG_ADVICE_FRAGMENT = "TAG_ADVICE_FRAGMENT";
+
+}
--- app/src/main/java/com/pushertest/www/budgetcatcher/View/Activity/MainActivity.java
+++ app/src/main/java/com/pushertest/www/budgetcatcher/View/Activity/MainActivity.java
@@ -6,7 +6,12 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
+import com.pushertest.www.budgetcatcher.Config;
 import com.pushertest.www.budgetcatcher.R;
+import com.pushertest.www.budgetcatcher.View.Fragment.Advice;
+import com.pushertest.www.budgetcatcher.View.Fragment.Catcher;
+import com.pushertest.www.budgetcatcher.View.Fragment.Home;
+import com.pushertest.www.budgetcatcher.View.Fragment.Manage;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,14 +21,45 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
+
                 case R.id.navigation_home:
+
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.container, new Home(), Config.TAG_HOME_FRAGMENT)
+                            .addToBackStack(null)
+                            .commit();
+
                     return true;
+
                 case R.id.navigation_catcher:
+
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.container, new Catcher(), Config.TAG_CATCHER_FRAGMENT)
+                            .addToBackStack(null)
+                            .commit();
+
                     return true;
+
                 case R.id.navigation_manage:
+
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.container, new Manage(), Config.TAG_MANAGE_FRAGMENT)
+                            .addToBackStack(null)
+                            .commit();
+
                     return true;
 
                 case R.id.navigation_advice:
+
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.container, new Advice(), Config.TAG_ADVICE_FRAGMENT)
+                            .addToBackStack(null)
+                            .commit();
+
                     return true;
 
                 case R.id.navigation_reports:
@@ -37,6 +73,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.container, new Home(), Config.TAG_HOME_FRAGMENT)
+                .addToBackStack(null)
+                .commit();
 
         BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
