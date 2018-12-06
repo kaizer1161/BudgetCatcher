ecd76d9261b05b9ca846972d9e745af099cf33ce
kaizer1161 <mir.rayan1@gmail.com>
2018-12-06 06:48:53 +0600

    launcher and splash screen updated

--- app/src/main/java/com/budgetcatcher/www/budgetcatcher/View/Activity/Launcher.java
+++ app/src/main/java/com/budgetcatcher/www/budgetcatcher/View/Activity/Launcher.java
@@ -6,27 +6,37 @@ import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.budgetcatcher.www.budgetcatcher.Config;
 import com.budgetcatcher.www.budgetcatcher.R;
 
 import butterknife.ButterKnife;
 
-public class SplashScreen extends AppCompatActivity {
+public class Launcher extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.splash_screen);
+        setContentView(R.layout.launcher_screen);
         ButterKnife.bind(this);
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
 
-                startActivity(new Intent(SplashScreen.this, Launcher.class));
-                finish();
+                if (getSharedPreferences(Config.SP_APP_NAME, MODE_PRIVATE).getBoolean(Config.SP_LOGGED_IN, false)) {
+
+                    startActivity(new Intent(Launcher.this, MainActivity.class));
+                    finish();
+
+                } else {
+
+                    startActivity(new Intent(Launcher.this, SignInSignUp.class));
+                    finish();
+
+                }
 
             }
-        }, 1000);
+        }, 2000);
 
     }
 
